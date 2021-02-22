31dac9c75e9146281a79ded0e3d6376f1f5fa95d
Lumenw <rmgroen89@gmail.com>
2021-02-22 15:47:05 +0100

    added github link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">weather app</div>;
+  return (
+    <div className="App">
+      weather app
+      <footer>
+        This
+        <a href="https://github.com/Lumenw/weather-app" target="_blank">
+          project
+        </a>
+        was coded by Rosa and is open sourced
+      </footer>
+    </div>
+  );
 }
